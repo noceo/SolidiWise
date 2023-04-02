14de0c785d6ec5364e80b521f8e1dcf2ad8007dc
noceo <paulschade20@gmail.com>
2023-04-02 13:39:11 -0500

    added test case for adding expenses

--- test/ExpenseList.test.js
+++ test/ExpenseList.test.js
@@ -1,9 +1,9 @@
 const ExpenseList = artifacts.require("ExpenseList");
+const truffleAssert = require("truffle-assertions");
 
 contract("ExpenseList", (accounts) => {
   before(async () => {
     this.expenseList = await ExpenseList.new(accounts[0], "SampleExpenseList", [accounts[1], accounts[2], accounts[3]]);
-    console.log(this.expenseList);
   });
 
   it("deploys successfully", async () => {
@@ -29,5 +29,38 @@ contract("ExpenseList", (accounts) => {
     const participants = await this.expenseList.getParticipants();
     assert.notEqual(participants.length, 0);
     assert.equal(participants.length, 3);
+  });
+
+  it("adds expenses to list", async () => {
+    // Check if require statements are correct
+    await truffleAssert.reverts(this.expenseList.addExpense("TestExpense", accounts[4], [accounts[1], accounts[2]], "0", ""), "Specified spender is not member of the list.");
+    await truffleAssert.reverts(this.expenseList.addExpense("TestExpense", accounts[0], [accounts[5], accounts[2]], "0", ""), "One or more of the specified debtors are not members of the list.");
+
+    // Create multiple expenses and add them to the list
+    for (let i = 1; i <= 5; i++) {
+      // Make a fake function call without modifying the state to verify that the return value is correct
+      const id = await this.expenseList.addExpense.call(`TestExpense${i}`, accounts[0], [accounts[1], accounts[2]], "0", "");
+      assert.equal(id.toNumber(), i);
+
+      // Make a "real" function call that modifies the state of the contract
+      const result = await this.expenseList.addExpense(`TestExpense${i}`, accounts[0], [accounts[1], accounts[2]], "0", "");
+
+      truffleAssert.eventEmitted(
+        result,
+        "LogNewExpense",
+        (event) => {
+          const id = event.id.toNumber() === i;
+          const name = event.name === `TestExpense${i}`;
+          const spender = event.spender === accounts[0];
+          // console.log(event.debtors);
+          const debtors = event.debtors[0] === accounts[1] && event.debtors[1] === accounts[2];
+          const mode = event.mode.toNumber() === 0;
+          const notes = event.notes === "";
+          // console.log("EVENT DETAILS: ", event.id.toNumber(), event.name, event.spender, event.mode, event.notes);
+          return id && name && spender && debtors && mode && notes;
+        },
+        "Contract should emit a correct LogNewExpense event."
+      );
+    }
   });
 });
